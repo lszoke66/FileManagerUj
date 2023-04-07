aaf4590fbbd070b905dafe4b68b018e7d186ec0b
László Szőke <lszoke@t-online.hu>
2023-04-07 17:01:01 +0200

    ss

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using FileManagerUj.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using System.Globalization;
+using FileManagerUj.Shared;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Localization;
+ using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SyncfusionLocalizer));
+           var supportedCultures = new[] { "en-US", "de", "fr", "ar", "zh" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures); 
+builder.Services.AddRazorPages();
+            builder.Services.AddControllers().AddNewtonsoftJson(options => { options.SerializerSettings.ContractResolver = new DefaultContractResolver(); });
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTQzNjMxNUAzMjMxMmUzMTJlMzMzNWlHVEh0T29VOHArOWNQaHBMUlZROWszb0UyOTVvYTR2ZmZCbEt4U0RKQzA9");
+// Configure the HTTP request pipeline.
+app.UseRequestLocalization(localizationOptions);
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+            app.UseCors();
+
+app.MapDefaultControllerRoute();
+            app.MapControllers();
+            app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
